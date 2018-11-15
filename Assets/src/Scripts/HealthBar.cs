ad9482890690bd74c69bd44ef33806b33f2a8f4e
Senhung Wong <alexwongsenhung@gmail.com>
2018-11-14 22:10:00 -0800

    major | Add demo health bar

---
+++ Assets/src/Scripts/HealthBar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    public float maxHealth;
+    public float currentHealth;
+
+    public float dropSpeed = 0.5f;
+
+    private float dropEffectPercentage = 1;
+
+    private Image healthBar;
+    private Image dropEffect;
+
+    private void Start() {
+        healthBar = transform.Find("Health").GetComponent<Image>();
+        dropEffect = transform.Find("DropEffect").GetComponent<Image>();
+    }
+
+    private void Update() {
+        float healthPercentage = Mathf.Min(Mathf.Max(0, currentHealth / maxHealth), 1);
+
+        /* Update health bar */
+        healthBar.fillAmount = healthPercentage;
+
+        /* Handle drop effect */
+        if (dropEffectPercentage > healthPercentage) {
+            dropEffectPercentage -= Time.deltaTime * dropSpeed;
+            dropEffect.fillAmount = dropEffectPercentage;
+        } else {
+            dropEffectPercentage = healthPercentage;
+        }
+    }
+
+}
